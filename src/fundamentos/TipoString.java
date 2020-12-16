90841a7082df043922049559548c85ee94cca186
Anderson Macedo <andersonmacedo1782@hotmail.com>
2020-12-16 13:20:35 -0300

    add tipo string

--- src/fundamentos/TipoString.java
+++ src/fundamentos/TipoString.java
@@ -3,6 +3,19 @@ package fundamentos;
 public class TipoString {
     public static void main(String[] args) {
         
-        System.out.println();
+        System.out.println("Anderson".charAt(0));
+
+        // obs. string é um objeto imutavel
+        String s = "Boa tarde";
+
+        System.out.println(s + "!!!");
+        System.out.println(s.startsWith("Boa"));
+        System.out.println(s.startsWith("Boa"));
+        System.out.println(s.endsWith("tarde"));
+        System.out.println(s.length());
+        System.out.println(s.equals("Boa tarde"));
+        System.out.println(s.equalsIgnoreCase("boa tarde"));
+
+
     }
 }
