150b3c8f3825a4730fb5e77736ec7aaca58d16e1
Anderson Macedo <andersonmacedo1782@hotmail.com>
2020-12-16 18:57:19 -0300

    Objeto vc tipos primitivos

---
+++ src/fundamentos/ObjetoVsPrimitivo.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class ObjetoVsPrimitivo {
+    public static void main(String[] args) {
+        
+        String nome = "Anderson";
+        //s. é um objeto pois tem propriedades e metodos
+
+        int idade = 33;
+        // idade. tipo primitivo nao tem propriedades e metodos
+
+        System.out.printf("Meu nome é %s e minha idade é %d", nome, idade);
+    }
+}
